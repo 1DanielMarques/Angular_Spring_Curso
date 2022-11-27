03f6b3996deee488b5e89c551569d18e3518514c
daniel <daniel.marques@edu.unifil.br>
2022-11-26 21:28:05 -0300

    entidade Course

--- crud-spring/src/main/java/com/daniel/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/daniel/crudspring/controller/CourseController.java
@@ -1,17 +1,18 @@
 package com.daniel.crudspring.controller;
 
-import org.springframework.http.ResponseEntity;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-public class HelloController {
+@RequestMapping(value = "/api/courses")
+public class CourseController {
     
-@GetMapping(value = "/test")
-public ResponseEntity<String> hello(){
-    return ResponseEntity.ok().body("Hello, World!");
-}
+    @GetMapping
+    public List<Object> findAll(){
+        return null;
+    }
 
 }
