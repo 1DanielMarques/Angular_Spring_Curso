93821a046d24a23dee26acede4d3253676ec8539
daniel <daniel.marques@edu.unifil.br>
2022-11-26 21:43:45 -0300

    retornando lista de course metodo GET

--- crud-spring/src/main/java/com/daniel/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/daniel/crudspring/CrudSpringApplication.java
@@ -1,13 +1,28 @@
 package com.daniel.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.daniel.crudspring.model.Course;
+import com.daniel.crudspring.repository.ICourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner initDataBase(ICourseRepository repository){
+		return args ->{
+			repository.deleteAll();
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+			repository.save(c);
+		};
 	}
 
 }
--- crud-spring/src/main/java/com/daniel/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/daniel/crudspring/controller/CourseController.java
@@ -2,19 +2,26 @@ package com.daniel.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daniel.crudspring.model.Course;
+import com.daniel.crudspring.repository.ICourseRepository;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping(value = "/api/courses")
+@AllArgsConstructor
 public class CourseController {
     
+    private final ICourseRepository repository;
+
     @GetMapping
     public List<Course> findAll(){
-        return null;
+        return repository.findAll();
     }
 
 }
