932e4749f1c0379153ba50bd50b7ab05bdc78672
daniel <daniel.marques@edu.unifil.br>
2022-11-30 17:33:49 -0300

    metodo Post

--- crud-spring/src/main/java/com/daniel/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/daniel/crudspring/controller/CourseController.java
@@ -2,8 +2,12 @@ package com.daniel.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daniel.crudspring.model.Course;
@@ -15,12 +19,18 @@ import lombok.AllArgsConstructor;
 @RequestMapping(value = "/api/courses")
 @AllArgsConstructor
 public class CourseController {
-    
+
     private final ICourseRepository repository;
 
     @GetMapping
-    public List<Course> findAll(){
+    public List<Course> findAll() {
         return repository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course c) {
+        return repository.save(c);
     }
 
 }
