e1122a3566fc270e857b0c8aa3b742cd936bf22c
daniel <daniel.marques@edu.unifil.br>
2022-11-30 17:32:49 -0300

    salvando cursos

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,7 +16,8 @@ export class CourseFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private _snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -27,16 +29,21 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value).subscribe(
-      (result) => console.log(result),
+      (result) => this.onSuccess(),
       (error) => this.onError()
     );
+    this.location.back();
   }
 
-  onCancel() {}
+  onCancel() {
+    this.location.back();
+  }
 
-private onError(){
-  this._snackBar.open('Erro ao salvar curso', '', { duration: 5000 });
-}
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso', '', { duration: 5000 });
+  }
 
-
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso', '', { duration: 5000 });
+  }
 }
